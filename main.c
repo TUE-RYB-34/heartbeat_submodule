f35ab999f61bfcfa5ff4bfc390ede2a19b39bba8
d0m1s2 <vrubliauskas.domas@gmail.com>
2025-11-27 11:28:36 +0100

    real heartbeat code with screen

--- main.c
+++ main.c
@@ -5,25 +5,40 @@
 #include <time.h>
 
 
-#define HEARTBEAT_SM_ADDR		0x52U
+#define HEARTBEAT_SM_ADDR        0x52U
 
-#define HEARTBEAT_SM_RMAP_SIZE	0x01U
+#define HEARTBEAT_SM_RMAP_SIZE    0x01U
 uint32_t rmap[HEARTBEAT_SM_RMAP_SIZE] = {
-    0x62656174UL,		// heart rate
+    0x15,        // heart rate
 };
 
-#define HEARTBEAT_SM_HEARTRATE_REG	0x00U
+#define HEARTBEAT_SM_HEARTRATE_REG    0x00U
 
+display_t display;
+FontxFile fx;
 
+void write_to_screen(uint32_t heart_rate)
+{
+    // Clear display
+    displayFillScreen(&display, RGB_BLACK);
+
+    int y = 20;
+    // Crying level
+    char heart_string[50];
+    snprintf(heart_string, sizeof(heart_string), "heart rate: %lu", (unsigned long)heart_rate);
+    displayDrawString(&display, &fx, 10, y, (uint8_t *)heart_string, RGB_GREEN);
+}
 
 int main(void) {
     // init
     pynq_init();
     switchbox_init();
 
+    InitFontx(&fx, "../../fonts/ILGH24XB.FNT", "");
+    display_init(&display);
     // pins
-    switchbox_set_pin(IO_PMODA3, SWB_IIC0_SCL);
-    switchbox_set_pin(IO_PMODA4, SWB_IIC0_SDA);
+    switchbox_set_pin(IO_AR_SCL, SWB_IIC0_SCL);
+    switchbox_set_pin(IO_AR_SDA, SWB_IIC0_SDA);
 
     // I2C
     iic_init(IIC0);
@@ -36,16 +51,18 @@ int main(void) {
     // loop
     for (;;) {
         // handle I2C
-		iic_slave_mode_handler(IIC0);
+        iic_slave_mode_handler(IIC0);
+        sleep_msec(1);
 
-		// sample sensor
+        write_to_screen(rmap[0]);
+        // sample sensor
         if (clock() - prev > 500000) {
-			// TODO: sample real data
-		}
+            // TODO: sample real data
+        }
 
-		// print data
+        // print data
         if (clock() - prev > 500000) {
-			// TODO: change screen
+            // TODO: change screen
 
             /*for (uint8_t i = 0; i < CRYING_SM_RMAP_SIZE; i++) {
                 printf("%ul", rmap[i]);
